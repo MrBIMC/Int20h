134b78b864638a3211ed5c1d3f4f691f64dd0e7a
Pavel Sikun <pavel.sikun@gmail.com>
2016-02-27 13:06:07 +0200

    Initial commit

---
+++ app/src/main/java/com/knightsofnull/int20h/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.knightsofnull.int20h
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
