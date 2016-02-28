86aeee6fcc1a92d8fde06711d453d0b27a7a9d75
Pavel Sikun <pavel.sikun@gmail.com>
2016-02-28 10:00:27 +0200

    new menu item layout

--- app/src/main/java/com/knightsofnull/int20h/shop/ShopFragment.kt
+++ app/src/main/java/com/knightsofnull/int20h/shop/ShopFragment.kt
@@ -3,6 +3,7 @@ package com.knightsofnull.int20h.shop
 import android.app.Fragment
 import android.content.DialogInterface
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.annotation.NonNull
 import android.support.design.widget.BottomSheetBehavior
