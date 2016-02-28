cb3c4d47a16cf4efbe643db6e03b6c7d3d56e583
Pavel Sikun <pavel.sikun@gmail.com>
2016-02-28 02:54:39 +0200

    missing icons

---
+++ app/src/main/java/com/knightsofnull/int20h/itemrequest/ItemRequestDialogFragment.kt
@@ -0,0 +1,10 @@
+package com.knightsofnull.int20h.itemrequest
+
+import android.support.v7.app.AppCompatDialogFragment
+
+/**
+ * Created by mrbimc on 28.02.16.
+ */
+class ItemRequestDialogFragment : AppCompatDialogFragment() {
+
+}
